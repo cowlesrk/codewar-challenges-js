d9a1d643ab3ddf9bc947c540be824517356410f6
cowlesrk <cowlesrk@yahoo.com>
2017-05-10 10:28:01 -0400

    Working on challenge 1 - having trouble finding the second lowest number in the array

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ Create a function that returns the sum of the two lowest positive numbers given 
 
 For example, when an array is passed like [19,5,42,2,77], the output should be 7. */
 
+const numbers = [19, 5, 42, 2, 77];
+
+const lowestNumber = Math.min(...numbers);
+const highestNumber = Math.max(...numbers);
+
+const secondLowest = (function() {
+	for (var i = 0; i < numbers.length; i++){
+		if (numbers[i] > lowestNumber && numbers[i] < highestNumber)
+			return i;
+	}
+}());
+
+
+document.write(lowestNumber);
+document.write(highestNumber);
+document.write(secondLowest);
+
+const sum = lowestNumber + secondLowest;
+document.write(sum);
